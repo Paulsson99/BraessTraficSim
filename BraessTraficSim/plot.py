d94821a48c05634c0b172eab5ee2971421067fd1
Simon Paulsson <simon@paulsson.one>
2022-11-21 16:16:21 +0100

    Selfish drivers

---
+++ BraessTraficSim/plot.py
@@ -0,0 +1,38 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+
+from  matplotlib.colors import LinearSegmentedColormap
+cmap=LinearSegmentedColormap.from_list('gr',["g", "r"], N=256) 
+
+
+RoadNetwork = dict[int, dict[int, tuple[float]]]
+
+options = {
+    'arrowstyle': '-|>',
+    'with_labels': True,
+    'arrows': True,
+    'width': 4,
+    'connectionstyle': 'arc3, rad = 0.1'
+}
+
+
+def draw_road_network(graph: RoadNetwork, traffic: np.ndarray):
+    G = nx.MultiDiGraph()
+    G.add_nodes_from(graph.keys())
+
+    print(traffic)
+
+    for i, connections in graph.items():
+        for j in connections.keys():
+            G.add_edge(i, j, weight=traffic[i, j])
+
+    edges, color = zip(*nx.get_edge_attributes(G, 'weight').items())
+
+    fig, ax = plt.subplots(1)
+    plot = nx.draw_circular(G, **options, edgelist=edges, edge_color=color, edge_cmap=cmap)
+    sm = plt.cm.ScalarMappable(cmap=cmap, norm=plt.Normalize(vmin=0, vmax=np.max(traffic)))
+    sm._A = []
+    fig.colorbar(sm)
+    plt.show()
+
