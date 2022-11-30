b437e6d360fdd517f57cb573bb16df65b73c700d
deric2019 <deric@student.chalmers.se>
2022-11-30 15:29:36 +0100

    Created a large network

--- BraessTraficSim/LargeNetwork.py
+++ BraessTraficSim/LargeNetwork.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import itertools
 from mycolorpy import colorlist as mcp
+from pprint import pprint
 
 
 class LargeNetwork:
@@ -28,12 +29,35 @@ class LargeNetwork:
         color = [self.layer_colors[data["layer"]] for v, data in self.G.nodes(data=True)]
         pos = nx.multipartite_layout(self.G, subset_key="layer")
         plt.figure(figsize=(8, 8))
-        nx.draw(self.G, pos, node_color=color, with_labels=False)
+        nx.draw(self.G, pos, node_color=color, with_labels=True)
         plt.axis("equal")
         plt.show()
 
+    def convert_to_graph_to_dict(self):
+        node_list = [node for node in self.G.nodes]
+        edge_list = [edge for edge in self.G.edges]
+
+        edge_index = 0
+        graph_dict = {}
+
+        for node in node_list:
+            edges_dict = {}
+
+            while node == edge_list[edge_index][0]:
+                to_edge = edge_list[edge_index][1]
+                edges_dict[to_edge] = (node, node)
+                if edge_index < len(edge_list) - 1:
+                    edge_index += 1
+                else:
+                    break
+            graph_dict[node] = edges_dict
+        return graph_dict
+
 
 if __name__ == '__main__':
-    subset_sizes = [5, 5, 4, 3, 2, 4, 4, 3]
-    G = LargeNetwork(subset_sizes)
-    G.plot()
+    subset_sizes = [1, 5, 4, 3, 2, 4, 4, 1]
+    large_network = LargeNetwork(subset_sizes)
+    graph = large_network.G
+    G = large_network.convert_to_graph_to_dict()
+    pprint(G)
+    # large_network.plot()
